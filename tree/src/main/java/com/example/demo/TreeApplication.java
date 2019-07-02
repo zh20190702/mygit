5b4e8b712997253e23995a55bc792864a1ed698a
zh20190702 <1782546222@qq.com>
2019-07-02 20:15:02 +0800

    eureka

--- tree/src/main/java/com/example/demo/TreeApplication.java
+++ tree/src/main/java/com/example/demo/TreeApplication.java
@@ -2,12 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class TreeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TreeApplication.class, args);
 	}
-
 }
